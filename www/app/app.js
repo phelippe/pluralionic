da231d070cf2e2ae3a83eb1d802833015c498ac1
Phelippe Matte <phelippematte@gmail.com>
2015-10-16 12:42:25 -0300

    primeiro commit

---
+++ www/app/app.js
@@ -0,0 +1,31 @@
+angular.module('eliteApp', ['ionic'])
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
+    })
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+
+            .state('home', {
+                abstract: true,
+                url: '/home',
+                templateUrl: 'app/home/home.html',
+            })
+
+            .state('app', {
+                url: '/app',
+                templateUrl: 'app/layout/menu-layout.html',
+            })
+        ;
+        $urlRouterProvider.otherwise('/home');
+    });
